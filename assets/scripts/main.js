d61a9d603a5e6d5f0387a8317d491c6a0785b910
afonsopacifer <afonsopolis@gmail.com>
2016-04-21 01:05:35 -0300

    Basic components created

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,60 @@
 var React    = require('react'),
 		ReactDOM = require('react-dom');
 
+var data = [
+	{id: 1, author: "Afonso", text: "heheh"},
+	{id: 2, author: "Raquel", text: "kkkkk"}
+];
+
+var CommentList = React.createClass({
+	render: function() {
+		var commentNodes = this.props.data.map(function(comment) {
+			return (
+				<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+				</Comment>
+			);
+		});
+		return (
+			<div>
+				{commentNodes}
+			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function(){
+		return (
+			<div>Comment Form</div>
+		)
+	}
+});
+
+var Comment = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<h2>{this.props.author}</h2>
+				{this.props.children}
+			</div>
+		)
+	}
+});
+
 var CommentBox = React.createClass({
 	render: function(){
-		return (<h1>Hello</h1>)
+		return (
+			<div>
+				<h1>Hello</h1>
+				<CommentList data={this.props.data}/>
+				<CommentForm/>
+			</div>
+		)
 	}
 });
 
 ReactDOM.render(
-	<CommentBox/>,
+	<CommentBox data={data}/>,
 	document.getElementById('app')
 );
